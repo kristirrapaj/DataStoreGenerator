e49ceab2bf9f76bed6b261de31c87383e6fdcaa2
Kristi Rrapaj <kristirapaj@gmail.com>
2023-12-22 10:35:30 +0100

    add possible database configurations in JSON file

---
+++ DS Generator/DS Generator/Database/DataBaseConfiguration.cs
@@ -0,0 +1,10 @@
+namespace DS_Generator.Database;
+
+public class DataBaseConfiguration
+{
+    public string ConnectionName { get; set; }
+    public string ConnectionString { get; set; }
+    public string Schema { get; set; }
+    public string DataProviderType { get; set; } 
+}
+
