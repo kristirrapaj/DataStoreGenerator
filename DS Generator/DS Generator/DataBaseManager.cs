0f46631f077242a49849ef5f77acbd2760020b54
Kristi Rrapaj <kristirapaj@gmail.com>
2023-12-27 10:22:45 +0100

    refactoring

--- DS Generator/DS Generator/DataBaseManager.cs
+++ DS Generator/DS Generator/DataBaseManager.cs
@@ -9,14 +9,8 @@ public class DatabaseManager
     public Dictionary<int, string> DataBaseNames { get; set; } = new Dictionary<int, string>();
     private string cnnStr;
     private string schema;
-    
-    //todo: Prendere questo path da UI
     private static string FolderPath;
     private static string FinalPath;
-    public DatabaseManager()
-    {
-        
-    }
     
     public void SetPaths(string configPath, string finalPath)
     {
@@ -28,12 +22,36 @@ public class DatabaseManager
         DataBaseNames.Add(0, "Seleziona un Database");
         foreach (string file in files)
         {
-            GetDataStoreType(file, index);
+            var dataStoreType = GetDataStoreProperties(file, "DATA_STORE_TYPE");
+            DataBaseNames.Add(index, dataStoreType);
             index++;
         }
     }
 
-    private void GetDataStoreType(string file, int index)
+    private string GetDataStoreProperties(string file, string tag)
+    {
+        var dataSet = new DataSet();
+        dataSet.ReadXml(file);
+
+        try
+        {
+            var value = dataSet.Tables[0].Rows[0][tag].ToString();
+
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new Exception("DataStoreType is null or empty");
+            }
+
+            return value;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    /*private void GetDataStoreType(string file, int index)
     {
         DataSet dataSet = new DataSet();
         dataSet.ReadXml(file);
@@ -57,31 +75,29 @@ public class DatabaseManager
             Console.WriteLine(e);
             throw;
         }
-    }
+    }*/
 
     public void DbSelector(int selectedIndex)
     {
-        if (selectedIndex != 0)
+        if (selectedIndex == 0) return;
+        
+        string[] tables = new string[] { "EXT_MAP_NOTE_POINT", "EXT_MAP_NOTE_LINE", "EXT_MAP_NOTE_POLY" };
+        ////
+        switch (DataBaseNames[selectedIndex])
         {
-            string[] tables = new string[] { "EXT_MAP_NOTE_POINT", "EXT_MAP_NOTE_LINE", "EXT_MAP_NOTE_POLY" };
-            ////
-            IDataStore dataStore;
-            switch (DataBaseNames[selectedIndex])
-            {
-                case "SQL_SERVER":
-                    getCnnStr();
-                    getSchema();
-                    dataStore = new DataStore.SQLServerDataStore.SQLServerGeomDataStore(connStr: cnnStr,
-                        schema: schema);
-                    dataStore.DataProviderType = "SQL_SERVER";
-                    var generator = new SqlGenerator(dataStore);
-                    generator.Generate(FinalPath, tables);
-                    break;
-            }
+            case "SQL_SERVER":
+                getCnnStr();
+                //GetSchema();
+                IDataStore dataStore = new DataStore.SQLServerDataStore.SQLServerGeomDataStore(connStr: cnnStr,
+                    schema: schema);
+                dataStore.DataProviderType = "SQL_SERVER";
+                var generator = new SqlGenerator(dataStore);
+                generator.Generate(FinalPath, tables);
+                break;
         }
     }
 
-    private void getSchema()
+    private void GetSchema(string file, int index)
     {
         //todo: leggere da xml config (oracle.xml/sql_server.xml) lo schema, quale xml? -> selected index
     }
