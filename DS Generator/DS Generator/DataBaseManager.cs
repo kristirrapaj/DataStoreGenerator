182ccb4e8ce24732f558d747c2d92a01c4bbf69d
ImAstyoo <pervan.daniele@gmail.com>
2023-12-29 12:57:38 +0100

    Updated at step 3)

--- DS Generator/DS Generator/DataBaseManager.cs
+++ DS Generator/DS Generator/DataBaseManager.cs
@@ -6,13 +6,14 @@ namespace DS_Generator;
 public class DataBaseManager {
     private const string FolderPath = @"../../../DataBaseConfig";
 
-    private List<string> _availableDataProvider;
+    private Tuple<int, string> _currentDataProvider;
+    private Tuple<int, string> _currentDatabase;
     private List<string> _availableDatabase;
+    private List<string> _availableDataProvider;
 
-    public DataBaseManager() {
-        _availableDatabase = new List<string>();
-        _availableDataProvider = new List<string>();
-        Initializer();
+    public List<string> AvailableDataProvider {
+        get => _availableDataProvider;
+        set => _availableDataProvider = value;
     }
 
     public List<string> AvailableDatabase {
@@ -22,30 +23,70 @@ public class DataBaseManager {
         }
     }
 
-    public List<string> AvailableDataProvider {
-        get => _availableDataProvider;
+    public DataBaseManager() {
+        _currentDataProvider = new Tuple<int, string>(-1, string.Empty);
+        _currentDatabase = new Tuple<int, string>(-1, string.Empty);
+        _availableDatabase = new List<string>();
+        _availableDataProvider = new List<string>();
+        AvailableDataProvider = [];
+        SetDatabasesConfig();
     }
 
-    public void SetDataProvider(int databaseTypeIndex) {
+    public void ChooseDatabase(int databaseIndex) {
+        if (databaseIndex < 0 || databaseIndex >= _availableDatabase.Count)
+            throw new InvalidDataException("index passed is: " + databaseIndex + ", which is invalid or out of range");
+        _currentDatabase = new Tuple<int, string>(databaseIndex, _availableDatabase[databaseIndex]);
+        SetAvailableDataProvider();
+    }
+
+    public void ChooseDataProvider(int dataProviderIndex) {
+        if (dataProviderIndex < 0 || dataProviderIndex >= _availableDataProvider.Count)
+            throw new InvalidDataException("index passed is: " + dataProviderIndex +
+                                           ", which is invalid or out of range");
+        _currentDataProvider = new Tuple<int, string>(dataProviderIndex, _availableDatabase[dataProviderIndex]);
+    }
+
+    private void SetAvailableDataProvider() {
         var dataProviderList = new List<string>();
-        var file = _availableDatabase[databaseTypeIndex];
+        var file = _currentDatabase.Item2;
         var dataSet = new DataSet();
         dataSet.ReadXml(file);
-        foreach (var str in dataSet.Tables[0].Rows)
-        {
-            Console.WriteLine(str);
+        foreach (DataRow dataProvider in dataSet.Tables[0].Rows) {
+            dataProviderList.Add(TagPickerXml(dataProvider, "ID"));
         }
-        _availableDataProvider = dataProviderList;
+
+        foreach (var sesso in dataProviderList) {
+            Console.WriteLine("3) Dani: " + AvailableDatabase[_currentDatabase.Item1] + ", " + sesso);
+        }
+        AvailableDataProvider = dataProviderList;
     }
 
-
-    private void Initializer() {
+    private void SetDatabasesConfig() {
         try {
             var files = Directory.GetFiles(FolderPath, "*.xml");
-            foreach (var file in files) _availableDatabase.Add(file);
+            foreach (var file in files) {
+                _availableDatabase.Add(file);
+                Console.WriteLine("1) Dani: " + file);
+            }
         }
         catch (Exception e) {
             Console.WriteLine(e);
+        }
+    }
+
+    private static string TagPickerXml(DataRow row, string tag) {
+        try {
+            var value = row[tag].ToString();
+
+            if (string.IsNullOrEmpty(value)) {
+                throw new Exception("Type is null or empty");
+            }
+
+            return value;
+        }
+        catch (Exception e) {
+            Console.WriteLine(e);
+            throw;
         }
     }
 }
