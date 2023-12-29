06b1afb4395ed1ace1a539244ae12e8243393594
ImAstyoo <pervan.daniele@gmail.com>
2023-12-29 11:39:27 +0100

    1) e half 2)

--- DS Generator/DS Generator/DataBaseManager.cs
+++ DS Generator/DS Generator/DataBaseManager.cs
@@ -1,106 +1,47 @@
 using System.Data;
 using System.IO;
-using DataStore.Interface;
-using DataStore.SQLServerDataStore;
-using DataFactory;
-using DataStore.Base;
-using DataStore.Factory;
-using DataStore.Interface;
 
 namespace DS_Generator;
 
-public class DatabaseManager
-{
-    private static string _folderPath;
-    private static string _finalPath;
+public class DatabaseManager {
+    private const string FolderPath = @"../../../DataBaseConfig";
 
-    //public Dictionary<int, Tuple<string, string, string>> DatabaseList = new Dictionary<int, Tuple<string, string, string>>();
+    private List<string> _availableDataProvider;
+    private List<string> _availableDatabase;
 
-    public Dictionary<int, Dictionary<string, DataSet>> DatabaseList =
-        new Dictionary<int, Dictionary<string, DataSet>>();
+    public DatabaseManager() {
+        _availableDatabase = new List<string>();
+        _availableDataProvider = new List<string>();
+        Initializer();
+    }
 
-    private string[] _tables;
+    public List<string> AvailableDatabase {
+        get {
+            return _availableDatabase.Select(db => db.Split("\\")[1].Split(".")[0])
+                .ToList(); // Trims the filepath returning only the file name (es: oracle instead of ../../../DataBaseConfig/oracle.xml)
+        }
+    }
 
-    private int _selectedIndex;
+    public List<string> AvailableDataProvider {
+        get => _availableDataProvider;
+    }
 
-    private void GetDatabaseProperties(int index, string dataStoreType, string file)
-    {
+    public void SetDataProvider(int databaseTypeIndex) {
+        var dataProviderList = new List<string>();
+        var file = _availableDatabase[databaseTypeIndex];
         var dataSet = new DataSet();
         dataSet.ReadXml(file);
+        _availableDataProvider = dataProviderList;
+    }
 
-        foreach (var key in DatabaseList.Keys)
-        {
-            if (key != index) continue;
-            foreach (var value in DatabaseList[key].Keys.Where(value => value == dataStoreType))
-            {
-                DatabaseList[key][value] = dataSet;
-            }
+
+    private void Initializer() {
+        try {
+            var files = Directory.GetFiles(FolderPath, "*.xml");
+            foreach (var file in files) _availableDatabase.Add(file);
         }
-    }
-
-    public void Initializer(string configPath, string finalPath)
-    {
-        _folderPath = configPath;
-        _finalPath = finalPath;
-
-        var files = Directory.GetFiles(_folderPath, "*.xml");
-        var index = 1;
-        
-        foreach (var file in files)
-        {
-            var dataStoreType = PickXmlProperty(file, "DATA_STORE_TYPE");
-
-            var dictionary = new Dictionary<string, DataSet>();
-            dictionary.Add(dataStoreType, new DataSet());
-
-            DatabaseList.Add(index, dictionary);
-            GetDatabaseProperties(index, dataStoreType, file);
-            index++;
-        }
-    }
-
-
-    public void IndexDbSelector(int selectedIndex)
-    {
-        if (selectedIndex == 0) return;
-        _selectedIndex = selectedIndex;
-        //todo: REFACTOR and IMPLEMENT
-        var factory = DataStoreFactory.GetDataStoreByDataProviderID()
-    }
-
-
-    public void SetTables(string[] tables)
-    {
-        _tables = tables;
-    }
-
-
-    //todo: IMPLEMENT
-    private static IDataStore CreateDataStore(string connectionString, string schema)
-    {
-        return new SQLServerGeomDataStore(connectionString, schema);
-    }
-
-    private static string PickXmlProperty(string file, string tag)
-    {
-        var dataSet = new DataSet();
-        dataSet.ReadXml(file);
-
-        try
-        {
-            var value = dataSet.Tables[0].Rows[0][tag].ToString();
-
-            if (string.IsNullOrEmpty(value))
-            {
-                throw new Exception("Type is null or empty");
-            }
-
-            return value;
-        }
-        catch (Exception e)
-        {
+        catch (Exception e) {
             Console.WriteLine(e);
-            throw;
         }
     }
 }
