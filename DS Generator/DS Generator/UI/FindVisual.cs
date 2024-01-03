9e385a48fe91bc3bf6d7236cd39d5d6573eb4cac
Kristi Rrapaj <kristirapaj@gmail.com>
2024-01-03 12:22:19 +0100

    really ugly working UI

---
+++ DS Generator/DS Generator/UI/FindVisual.cs
@@ -0,0 +1,43 @@
+using System.Windows;
+using System.Windows.Media;
+
+namespace DS_Generator.UI;
+
+public class FindVisual
+{
+    public T FindVisualChild<T>(DependencyObject parent, string name) where T : FrameworkElement
+    {
+        if (parent == null)
+            return null;
+
+        if (parent is T frameworkElement && frameworkElement.Name == name)
+        {
+            return frameworkElement;
+        }
+
+        T foundChild = null;
+        int childCount = VisualTreeHelper.GetChildrenCount(parent);
+
+        for (int i = 0; i < childCount; i++)
+        {
+            DependencyObject child = VisualTreeHelper.GetChild(parent, i);
+            foundChild = FindVisualChild<T>(child, name);
+
+            if (foundChild != null)
+                break;
+        }
+
+        return foundChild;
+    }
+
+    
+    public T FindVisualParent<T>(DependencyObject? child) where T : DependencyObject
+    {
+        while (child != null && !(child is T))
+        {
+            child = VisualTreeHelper.GetParent(child);
+        }
+
+        return child as T;
+    }
+}
