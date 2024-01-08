0912dd039f7c8c1ac116d6eb597218cee3b253b1
Kristi Rrapaj <kristirapaj@gmail.com>
2024-01-08 12:10:20 +0100

    documentation for the controller

--- DS Generator/DS Generator/UI/MainWindowController.cs
+++ DS Generator/DS Generator/UI/MainWindowController.cs
@@ -5,34 +5,63 @@ using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace DS_Generator.UI;
 
+/// <summary>
+/// Controls the main operations and user interactions in the DS_Generator application.
+/// Manages database configurations, table selections, and UI updates.
+/// </summary>
 public class MainWindowController {
     private List<string> mSelectedTables;
     private DataBaseManager mDataBaseManager;
-
+    
+    /// <summary>
+    /// Initializes a new instance of the MainWindowController class.
+    /// Sets up the necessary data structures and initializes the DataBaseManager.
+    /// </summary>
     public MainWindowController() {
         mSelectedTables = [];
         mDataBaseManager = new DataBaseManager();
     }
 
+    /// <summary>
+    /// Gets the list of available datastores from the DataBaseManager.
+    /// </summary>
     public List<string> GetAvaliableDatastores => mDataBaseManager.AvailableDatastores;
+    
+    /// <summary>
+    /// Gets the list of available tables from the DataBaseManager.
+    /// </summary>
     public List<string> GetTablesList => mDataBaseManager.AvailableTables;
-
+    
+    /// <summary>
+    /// Sets the output directory in the DataBaseManager for generated data.
+    /// </summary>
+    /// <param name="directory">The directory to set as the output path.</param>
     private void SetOutputDirectory(string directory) {
         mDataBaseManager.OutputConfigFilePath = $"{directory}";
     }
 
-
-    // CHIAMALO QUANDO L'USER SELEZIONA UNA DATABASE
-    // IN AUTOMATICO SETTA LE TABELLE DISPONIBILI
+    /// <summary>
+    /// Sets the database configuration in the DataBaseManager.
+    /// Called when a user selects a database.
+    /// </summary>
+    /// <param name="database">The database to set.</param>
     public void SetDatabase(string database) {
         mDataBaseManager.Database = database;
     }
 
-    // CHIAMALO QUANDO L'USER HA SELEZIONATO LE TABELLE ED È PRONTO PER GENERARE
+    /// <summary>
+    /// Sets the tables to be processed in the DataBaseManager.
+    /// Called when a user has finished selecting tables and is ready to generate the XML files.
+    /// </summary>
     public void SetTables() {
         mDataBaseManager.Tables = mSelectedTables.ToArray();
     }
 
+    ///<summary>
+    /// Modifies the list of selected tables based on user commands (add or remove).
+    /// </summary>
+    /// <param name="table">The table to be added or removed.</param>
+    /// <param name="command">The command indicating whether to add or remove the table.</param>
     public void ModifySelectedTables(string table, string command) {
         switch (command) {
             case "ADD":
@@ -44,6 +73,13 @@ public class MainWindowController {
         }
     }
 
+    /// <summary>
+    /// Updates the text in a RichTextBox with the specified message and color.
+    /// Used for displaying status messages and errors in the console part of the UI.
+    /// </summary>
+    /// <param name="richTextBox">The RichTextBox to update.</param>
+    /// <param name="text">The text message to display.</param>
+    /// <param name="color">The color of the text message.</param>
     public void ChangeConsoleText(RichTextBox richTextBox, string text, Brush color) {
         Paragraph paragraph = new Paragraph();
         paragraph.Inlines.Add(new Run(text));
@@ -52,12 +88,21 @@ public class MainWindowController {
         richTextBox.Document.Blocks.Add(paragraph);
     }
 
-
+    /// <summary>
+    /// Sets the configuration file path in the DataBaseManager.
+    /// Fetches database configurations based on the provided configuration file.
+    /// </summary>
+    /// <param name="path">The path to the configuration file.</param>
     private void SetConfigFilePath(string path) {
         mDataBaseManager.ConfigFilePath = path;
         mDataBaseManager.FetchDatabasesFromConfigurationFile();
     }
-
+    
+    /// <summary>
+    /// Creates and shows a dialog to the user for selecting files or directories.
+    /// </summary>
+    /// <param name="type">The type of dialog to show ("XML" for file selection, "DIRECTORY" for folder selection).</param>
+    /// <returns>The path selected by the user.</returns>
     public string DialogCreator(string type) {
         CommonOpenFileDialog dialog;
         switch (type) {
